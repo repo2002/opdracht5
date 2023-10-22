60b00badf11f20f894b47f1e6737d9b24162424c
repo2002 <Refael.michelashvili@student.kdg.be>
2023-10-22 23:26:16 +0200

    examen js als bestaande opdracht toegevoegd aan deze oefening

---
+++ scripts/opdracht3.js
@@ -0,0 +1,19 @@
+// Put your code here
+// import 'cropperjs/dist/cropper.css';
+import Cropper from 'cropperjs';
+
+const image = document.getElementById('image');
+const cropper = new Cropper(image, {
+  aspectRatio: 16 / 9,
+  crop(event) {
+    console.log(event.detail.x);
+    console.log(event.detail.y);
+    console.log(event.detail.width);
+    console.log(event.detail.height);
+    console.log(event.detail.rotate);
+    console.log(event.detail.scaleX);
+    console.log(event.detail.scaleY);
+  },
+});
+cropper.zoom(0.1);
+cropper.zoom(-0.1);
---
+++ scripts/opdracht4.js
@@ -0,0 +1,8 @@
+// Simple list
+Sortable.create(list, { /* options */ });
+
+// List with handle
+Sortable.create(listWithHandle, {
+  handle: '.glyphicon-move',
+  animation: 150
+});
